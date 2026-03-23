5b6128e75237a08f03d09e254cdc60654426359e
Gabriel Ruh-Mosquera <garuhmosquera@gmail.com>
2026-03-23 06:59:44 -0400

    transfer

---
+++ patient-service/src/main/java/com/patientmng/patientservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.patientmng.patientservice.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException e) {
+        Map<String, String> errors = e
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(
+                        Collectors.toMap(
+//                                calls the get field and get msg functions on each field error
+                                FieldError::getField,
+                                FieldError::getDefaultMessage)
+                );
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
