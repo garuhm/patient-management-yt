bf55c4fe0b49c3b706670ee6d45064e5755b8e25
Gabriel Ruh-Mosquera <garuhmosquera@gmail.com>
2026-03-28 22:17:09 -0400

    patient service protected by auth service and api gateway

---
+++ api-gateway/src/main/java/com/patientmng/apigateway/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.patientmng.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+}
